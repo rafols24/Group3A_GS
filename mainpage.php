e08ac1b50a54945a55f3c95303f7aacc2b9f2b70
rafols24 <emelisarafols@gmail.com>
2021-04-19 18:37:20 +0800

    changes by rafols

---
+++ mainpage.php
@@ -0,0 +1,30 @@
+<?php include_once("function.php")?>
+<!DOCTYPE>
+<html>
+    <head>
+        <title> Vertical Menu </title>
+        <meta name ="viewport" content ="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <script src ="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src ="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+        <link rel = "stylesheet" href = "index.css">
+        </head>
+        <body>
+        <div class = "container">
+        <div class="d-flex justify-content-center h-100">
+            <div class = "card1">
+                <div class = "dashboard">
+                     <center><p>Inventory</p><hr><br>
+                     <p class = "dash"><img id=dash src="imgs/dash1.png"> Dashboard</p>
+
+                    </center>
+                       
+                </div>
+
+            </div>
+        </div>
+        </div>
+       
+        </body>
+</html>
+
