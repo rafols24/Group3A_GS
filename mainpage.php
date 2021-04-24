dde76240f2bd1f45dc9f412166650f25e81fb6cb
rafols24 <emelisarafols@gmail.com>
2021-04-24 12:02:30 +0800

    rafols

--- mainpage.php
+++ mainpage.php
@@ -9,20 +9,11 @@
         <!-- <script src ="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script> -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" 
         integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-    <!-- Font Awesome JS -->
-        <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" 
-        integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
-        <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" 
-        integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <!-- Popper.JS -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" 
-        integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-        <!-- Bootstrap JS -->
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" 
-        integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
         <link rel = "stylesheet" href = "mainpage.css">
+        <!-- footerRafols -->
+        <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link href="StyleSheet.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         </head>
         <style>
       
@@ -34,97 +25,106 @@
             <div class="sidebar-header">
                 <h3 class="text-center">HOPIA DE TRES INVENTORY</h3>
             </div>
-            <ul class="list-unstyled components">
-                <p class="text-center">DASHBOARD</p>
+            <ul class="list-unstyled components" >
+                <p class="text-center"><DASHBOARD><img id= "dash" src = "imgs/dash.png">&nbsp;DASHBOARD</p>
                 <li>
-                    <a href="#">TOP SELLING</a>
+                    <a href="#" ><img id= "dash" src = "imgs/cart.png">&nbsp;TOP SELLING</a><br>
                 </li>
                 <li>
-                    <a href="#">INCOME</a>
+                    <a href="#"><img id= "dash" src = "imgs/income.png">&nbsp;INCOME</a><br>
                 </li>
                 <li>
-                    <a href="#">EXPENSES</a>
+                    <a href="#"><img id= "dash" src = "imgs/expense.png">&nbsp;EXPENSES</a><Br>
                 </li>
                 <li>
-                    <a href="#">FINANCIAL PERFORMANCES</a>
+                    <a href="#"><img id= "dash" src = "imgs/finance.png">&nbsp;FINANCE STATUS</a><Br>
                 </li>
                 <li>
-                    <a href="#">INCOMING DELIVERY</a>
+                    <a href="#"><img id= "dash" src = "imgs/incoming.png">&nbsp;INCOMING DELIVERY</a><br>
                 </li>
                 <li>
-                    <a href="#">RECEIVED DELIVERY</a>
+                    <a href="#"><img id= "dash" src = "imgs/delivered.png">&nbsp;RECEIVED DELIVERY</a>
                 </li>
+            
             </ul>
         </nav>
         <div class="container mt-2">
-        <div class="card">
-        <div class="col-sm-9">
+        <div class="col-sm-12" id = "admin">
       <div class="well">
-        <h4 >Dashboard</h4>
-        <p>Some text..</p>
+        <a >ADMIN&nbsp;<img id= "dash" src = "imgs/admin.png"></a>
       </div>
       </div>
       </div>
-      </div>
+      
     <!-- Page Content -->
     <div id="content">
     </div>
 
     </div>
 
-      <!-- Site footer -->  
-      <footer class="site-footer" style="margin-left:264px">
-      <div class="container justify-content-center" >
-        <div class="row">
-          <div class="col-sm-12 col-md-6">
-            <h6>About</h6>
-            <p class="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming programmers with the code. Scanfcode focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
-          </div>
+       <!-- footer -->
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-xs-12">
+                    <div class="first">
+                        <h4>My Skills</h4>
+                        <p> Analytical Skills</p>
+                        <p> Problem-solving skills</p>
+                        <p> Critical-thinking skills</p>
+                        <p> Detail-oriented</p>
+                        <p> Multitasking</p>
+                        <p> Self-motivated</p>
+                    </div>
+                </div>
 
-          <div class="col-xs-6 col-md-3">
-            <h6>Categories</h6>
-            <ul class="footer-links">
-              <li><a href="http://scanfcode.com/category/c-language/">C</a></li>
-              <li><a href="http://scanfcode.com/category/front-end-development/">UI Design</a></li>
-              <li><a href="http://scanfcode.com/category/back-end-development/">PHP</a></li>
-              <li><a href="http://scanfcode.com/category/java-programming-language/">Java</a></li>
-              <li><a href="http://scanfcode.com/category/android/">Android</a></li>
-              <li><a href="http://scanfcode.com/category/templates/">Templates</a></li>
-            </ul>
-          </div>
+                <div class="col-md-4 col-xs-12">
+                    <div class="second">
+                        <h4> Navigate</h4>
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Projects</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
 
-          <div class="col-xs-6 col-md-3">
-            <h6>Quick Links</h6>
-            <ul class="footer-links">
-              <li><a href="http://scanfcode.com/about/">About Us</a></li>
-              <li><a href="http://scanfcode.com/contact/">Contact Us</a></li>
-              <li><a href="http://scanfcode.com/contribute-at-scanfcode/">Contribute</a></li>
-              <li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>
-              <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
-            </ul>
-          </div>
+                <div class="col-md-4 col-xs-12">
+                    <div class="third">
+                        <h4> Contact</h4>
+                        <ul>
+                            <li>Andreea Mihaela Bunget </li>
+                            <li></li>
+
+
+                          <li><i class="far fa-envelope"></i> andreea@andreeabunget.co.uk</li>
+                            <li><i class="far fa-envelope"></i> email@yahoo.com</li>
+
+
+                          <li><i class="fas fa-map-marker-alt"></i> London, UK </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
-        <hr>
-      </div>
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8 col-sm-6 col-xs-12">
-            <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by 
-         <a href="#">Scanfcode</a>.
-            </p>
-          </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="line"></div>
+                    <div class="second2">
+                        <a href="https://codepen.io/AndreeaBunget" target="_blank"> <i class="fab fa-codepen fa-2x margin"></i></a>
+                        <a href="https://github.com/WebDeveloperCodeRep" target="_blank"> <i class="fab fa-github fa-2x margin"></i></a>
+                        <a href="https://www.linkedin.com/in/andreea-mihaela-bunget-a4248812b/" target="_blank"> <i class="fab fa-linkedin fa-2x margin"></i></a>
+                        <a href="https://www.youtube.com/channel/UCX674BUbomzBCakbb75lhfA?view_as=subscriber" target="_blank"><i class="fab fa-youtube fa-2x margin" ></i></a>
 
-          <div class="col-md-4 col-sm-6 col-xs-12">
-            <ul class="social-icons">
-              <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
-              <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
-              <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
-              <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   
-            </ul>
-          </div>
-        </div>
-      </div>
-</footer>
+                    </div>
+
+                </div>
+            </div>
+            </div>
+    </div>
+   
     </body>
 </html>
 
