250cfbc98cada2df63fec53ebab8f4f12908d914
rafols24 <emelisarafols@gmail.com>
2021-05-02 21:38:05 +0800

    initial

---
+++ purchases.php
@@ -0,0 +1,97 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
+    crossorigin="anonymous">
+  <link rel="stylesheet" href="css/main.css">
+  <title>Fancy Form</title>
+</head>
+
+<body>
+<br>    
+<center><h1>PURCHASES</h1></center>
+  <!-- table -->
+  <div class="container">
+	<div class="row">
+		<table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>FLAVOR</th>
+        <th>NO. of BOXES</th>
+        <th>AMOUNT</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>UBE</td>
+        <td></td>
+        <td></td>
+        
+      </tr>
+      <tr>
+        <td>MONGO</td>
+        <td></td>
+        <td></td>
+        
+      </tr>
+      <tr>
+        <td>CAMOTE</td>
+        <td></td>
+        <td></td>
+      </tr>
+
+      <tr>
+        <td>STRAWBERRY</td>
+        <td></td>
+        <td></td>
+      </tr>
+    </tbody>
+       
+      <tr>
+        <td>BABOY</td>
+        <td></td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
+	</div>
+</div>
+<!-- end of table -->
+</div>
+
+</body>
+</html>
+
+<!-- css -->
+<style>
+@import url("https://fonts.googleapis.com/css?family=Pacifico|Roboto");
+.table{
+    background-color:#fff;
+    box-shadow:0px 2px 2px #aaa;
+    margin-top:50px;
+}
+body {
+  background: white;
+  color:black;
+  font-family: 'Roboto', sans-serif;
+  margin: 0; }
+
+#container {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: top;
+  align-items: center; }
+    </style>
+    <!-- js -->
+    <script>
+    </script>
