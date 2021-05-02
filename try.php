c64b0106e78be5494a07008fb701925932abd6f6
storm-keith <dejitoloyla.00@gmail.com>
2021-05-02 11:26:03 +0800

    new commit

--- try.php
+++ try.php
@@ -11,7 +11,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="try.css">
 <script>
     function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
@@ -104,6 +105,115 @@
         color: #212422;
     }
 
+    #top-selling{
+        margin-top: 5%;
+    }
+
+    /*----  Main Style  ----*/
+#cards_landscape_wrap-2{
+
+  background: #F7F7F7;
+}
+#cards_landscape_wrap-2 .container{
+  padding-top: 80px; 
+  margin-left: 14%;
+
+}
+#cards_landscape_wrap-2 a{
+  text-decoration: none;
+  outline: none;
+}
+
+#cards_landscape_wrap-2 .card-flyer .image-box{
+  background: #ffffff;
+  overflow: hidden;
+  box-shadow: 0px 2px 15px rgba(0, 0, 0, 0.50);
+
+}
+#cards_landscape_wrap-2 .card-flyer .image-box img{
+  -webkit-transition:all .9s ease; 
+  -moz-transition:all .9s ease; 
+  -o-transition:all .9s ease;
+  -ms-transition:all .9s ease; 
+  width: 100%;
+  height: 300px;
+}
+/* #cards_landscape_wrap-2 .card-flyer:hover .image-box img{
+  opacity: 0.7;
+  -webkit-transform:scale(1.15);
+  -moz-transform:scale(1.15);
+  -ms-transform:scale(1.15);
+  -o-transform:scale(1.15);
+  transform:scale(1.15);
+} */
+
+#cards_landscape_wrap-2 .card-flyer .text-box .text-container{
+  padding: 30px 18px;
+}
+#cards_landscape_wrap-2 .card-flyer{
+  background: #FFFFFF;
+  /* -webkit-transition: all 0.2s ease-in;
+  -moz-transition: all 0.2s ease-in;
+  -ms-transition: all 0.2s ease-in;
+  -o-transition: all 0.2s ease-in;
+  transition: all 0.2s ease-in; */
+  box-shadow: 0px 3px 4px rgba(0, 0, 0, 0.40);
+}
+
+
+
+#cards_landscape_wrap-2 .card-flyer:hover{
+  background: #fff;
+  box-shadow: 0px 15px 26px rgba(0, 0, 0, 0.50);
+  /* -webkit-transition: all 0.2s ease-in;
+  -moz-transition: all 0.2s ease-in;
+  -ms-transition: all 0.2s ease-in;
+  -o-transition: all 0.2s ease-in;
+  transition: all 0.2s ease-in; */
+ 
+}
+#cards_landscape_wrap-2 .card-flyer .text-box p{
+  margin-bottom: 0px;
+  padding-bottom: 0px; 
+  font-size: 14px;
+  letter-spacing: 1px;
+  color: #000000;
+}
+#cards_landscape_wrap-2 .card-flyer .text-box h6{
+  margin-top: 0px;
+  margin-bottom: 4px; 
+  font-size: 18px;
+  font-weight: bold;
+  text-transform: uppercase;
+  font-family: 'Roboto Black', sans-serif;
+  letter-spacing: 1px;
+  color: #00acc1;
+}
+
+#more {
+    display: none;
+}
+
+.checked {
+  color: orange;
+}
+
+
+.card button {
+  border: none;
+  outline: 0;
+  padding: 12px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+  font-size: 18px;
+}
+
+.card button:hover {
+  opacity: 0.7;
+}
 </style>
 
 </head>
@@ -136,12 +246,203 @@
 
     <div id="main">
         <section id="top-selling">
-                TOP SELLING <br>
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
-                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            <h1>HOPIA DE TRES' BEST OF SALES </h1>
+            <!-- PRODUCT CARDS -->
+                <div id="cards_landscape_wrap-2">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                                <a href="">
+                                    <div class="card-flyer">
+                                        <div class="text-box">
+                                            <div class="image-box" style="position: relative;">
+                                                <img src="https://ph-test-11.slatic.net/p/385dd5173c2d5c31441122ce13dad22c.jpg" alt="" style="display: block;"/>
+                                                <i class="material-icons" style="font-size:48px;color:black;position:absolute;bottom:84%; left:0;">filter_1</i>
+                                            </div>
+                                            <div class="card">
+                                                <h6 class="mt-3 text-center" style="font-size:27px;">UBE HOPIA</h6>
+                                                <p class="price text-center" style="font-size:25px;color:#5C5454;font-weight:bold">$19.99</p>
+                                                <p class="p-3">Lorem Ipsum is simply dummy text of the printing ahsuhsjha.</p>
+                                                <p class="mb-3"><span class="fa fa-star checked float-left ml-4"></span>
+                                                <span class="fa fa-star checked float-left ml-1"></span>
+                                                <span class="fa fa-star checked float-left ml-1"></span>
+                                                <span class="fa fa-star float-left ml-1"></span>
+                                                <span class="fa fa-star float-left ml-1"></span></p>                   
+                                                <p><button>See Sales Report</button></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                                <a href="">
+                                    <div class="card-flyer">
+                                        <div class="text-box">
+                                            <div class="image-box" style="position: relative;">
+                                                <img src="https://cdn.shopify.com/s/files/1/0451/1016/5671/products/2040x2048_HH_mongospecial_2040x.jpg?v=1618542703" alt="" style="display: block;"/>
+                                                <i class="material-icons" style="font-size:48px;color:#FF0000;position: absolute;bottom:84%; left:0;">filter_2</i>
+                                            </div>
+                                            <div class="card">
+                                                <h6 class="mt-3 text-center" style="font-size:26px;">HOPIANG MONGO</h6>
+                                                <p class="price text-center" style="font-size:25px;color:#5C5454;font-weight:bold">$19.99</p>
+                                                <p class="p-3">Lorem Ipsum is simply dummy text of the printing ahsuhsjha.</p>
+                                                <p class="mb-3"><span class="fa fa-star checked float-left ml-4"></span>
+                                                <span class="fa fa-star checked float-left ml-1"></span>
+                                                <span class="fa fa-star checked float-left ml-1"></span>
+                                                <span class="fa fa-star float-left ml-1"></span>
+                                                <span class="fa fa-star float-left ml-1"></span></p>                   
+                                                <p><button>See Sales Report</button></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                                <a href="">
+                                    <div class="card-flyer">
+                                        <div class="text-box">
+                                            <div class="image-box" style="position: relative;">
+                                                <img src="https://cf.shopee.ph/file/6a15f06c53f587d039ddcb585224370a" alt="" style="display: block;"/>
+                                                <i class="material-icons" style="font-size:48px;color:#FF0000;position: absolute;bottom:84%; left:0;">filter_3</i>
+                                            </div>
+                                            <div class="card">
+                                                <h6 class="mt-3 text-center" style="font-size:26px;">HOPIANG PRESA</h6>
+                                                <p class="price text-center" style="font-size:25px;color:#5C5454;font-weight:bold">$19.99</p>
+                                                <p class="p-3">Lorem Ipsum is simply dummy text of the printing ahsuhsjha.</p>
+                                                <p class="mb-3"><span class="fa fa-star checked float-left ml-4"></span>
+                                                <span class="fa fa-star checked float-left ml-1"></span>
+                                                <span class="fa fa-star checked float-left ml-1"></span>
+                                                <span class="fa fa-star float-left ml-1"></span>
+                                                <span class="fa fa-star float-left ml-1"></span></p>                   
+                                                <p><button>See Sales Report</button></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>  
+          <!-- END PRODUCT CARDS  -->
+            <!-- PRODUCT TABLE -->
+            <div class="container" style="width:60%">
+                <div class="cart transition is-open">
+    
+    <div class="table">
+      
+      <div class="layout-inline row th">
+        <div class="col col-pro">Product</div>
+        <div class="col col-price align-center "> 
+          Price
+        </div>
+        <div class="col col-qty align-center">QTY</div>
+        <div class="col">VAT</div>
+        <div class="col">Total</div>
+      </div>
+      
+      <div class="layout-inline row">
+        
+        <div class="col col-pro layout-inline">
+          <img src="http://static.ddmcdn.com/gif/10-kitten-cuteness-1.jpg" alt="kitten" />
+          <p>Happy Little Critter</p>
+        </div>
+        
+        <div class="col col-price col-numeric align-center ">
+          <p>£59.99</p>
+        </div>
+
+        <div class="col col-qty layout-inline">
+          <a href="#" class="qty qty-minus">-</a>
+            <input type="numeric" value="3" />
+          <a href="#" class="qty qty-plus">+</a>
+        </div>
+        
+        <div class="col col-vat col-numeric">
+          <p>£2.95</p>
+        </div>
+        <div class="col col-total col-numeric">               <p> £182.95</p>
+        </div>
+      </div>
+      
+      <div class="layout-inline row row-bg2">
+
+        <div class="col col-pro layout-inline">
+          <img src="http://lovemeow.com/wp-content/uploads/2012/05/kitten81.jpg" alt="kitten" />
+          <p>Scared Little Kittie</p>
+        </div>
+        
+        <div class="col col-price col-numeric align-center ">
+          <p>£23.99</p>
+        </div>
+
+        <div class="col col-qty  layout-inline">
+          <a href="#" class="qty qty-minus ">-</a>
+            <input type="numeric" value="1" />
+          <a href="#" class="qty qty-plus">+</a>
+        </div>
+        
+        <div class="col col-vat col-numeric">
+          <p>£1.95</p>
+        </div>
+        <div class="col col-total col-numeric">
+          <p>£25.94</p>
+        </div>      
+      
+      </div>
+      
+       <div class="layout-inline row">
+        
+        <div class="col col-pro layout-inline">
+          <img src="http://cdn.cutestpaw.com/wp-content/uploads/2012/04/l-my-first-kitten.jpg" alt="kitten" />
+          <p>Curious Little Begger</p>
+        </div>
+        
+        <div class="col col-price col-numeric align-center ">
+          <p>£59.99</p>
+        </div>
+
+        <div class="col col-qty layout-inline">
+          <a href="#" class="qty qty-minus">-</a>
+            <input type="numeric" value="3" />
+          <a href="#" class="qty qty-plus">+</a>
+        </div>
+        
+        <div class="col col-vat col-numeric">
+          <p>£2.95</p>          
+        </div>
+         <div class="col col-total col-numeric">  
+           <p>£182.95</p>
+         </div>         
+      </div>
+  
+       <div class="tf">
+         <div class="row layout-inline">
+           <div class="col">
+             <p>VAT</p>
+           </div>
+           <div class="col"></div>
+         </div>
+         <div class="row layout-inline">
+           <div class="col">
+             <p>Shipping</p>
+           </div>
+           <div class="col"></div>
+         </div>
+          <div class="row layout-inline">
+           <div class="col">
+             <p>Total</p>
+           </div>
+           <div class="col"></div>
+         </div>
+       </div>         
+  </div>
+    
+    <a href="#" class="btn btn-update">Update cart</a>
+  
+</div>
+<!-- END PRODUCT TABLE -->
+
         </section>
+        </span>    
         <br>
         <br>
         <section id="income">
@@ -213,6 +514,23 @@
         var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
         chart.draw(data, options);
       }
+
+      
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
     </script>
 </body>
 </html> 
